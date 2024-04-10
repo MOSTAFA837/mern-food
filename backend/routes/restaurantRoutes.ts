3eee298c655b174f0a59b1e4f68b5135411db6c7
MOSTAFA837 <mostafamanchester837@gmail.com>
2024-04-10 02:47:21 +0200

    search api

--- backend/routes/restaurantRoutes.ts
+++ backend/routes/restaurantRoutes.ts
@@ -5,8 +5,10 @@ import {
   createRestaurant,
   getRestaurant,
   updateRestaurant,
+  searchRestaurants,
 } from "../controllers/restuantController";
 import { validateRestaurantRequest } from "../middleware/validation";
+import { param } from "express-validator";
 
 const router = express.Router();
 
@@ -36,6 +38,16 @@ router.put(
   jwtCheck,
   jwtParse,
   updateRestaurant
+);
+
+router.get(
+  "/search/:city",
+  param("city")
+    .isString()
+    .trim()
+    .notEmpty()
+    .withMessage("City parameter must be a valid string."),
+  searchRestaurants
 );
 
 export default router;
