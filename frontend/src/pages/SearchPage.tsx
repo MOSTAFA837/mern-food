94f495e26df2a7db4d57f14bf292318bcc0387b8
MOSTAFA837 <mostafamanchester837@gmail.com>
2024-04-12 01:14:24 +0200

    cuisine filter

--- frontend/src/pages/SearchPage.tsx
+++ frontend/src/pages/SearchPage.tsx
@@ -1,4 +1,5 @@
 import { useSearchRestaurants } from "@/api/restaurantApi";
+import CuisineFilter from "@/components/CuisineFilter";
 import PaginationSelector from "@/components/PaginationSelector";
 import SearchBar, { SearchForm } from "@/components/SearchBar";
 import SearchResultCard from "@/components/SearchResultCard";
@@ -9,13 +10,16 @@ import { useParams } from "react-router-dom";
 export type SearchState = {
   searchQuery: string;
   page: number;
+  selectedCuisines: string[];
 };
 
 export default function SearchPage() {
   const { city } = useParams();
+  const [isExpanded, setIsExpanded] = useState<boolean>(false);
   const [searchState, setSearchState] = useState<SearchState>({
     searchQuery: "",
     page: 1,
+    selectedCuisines: [],
   });
 
   const { results, isLoading } = useSearchRestaurants(searchState, city);
@@ -51,9 +55,26 @@ export default function SearchPage() {
     }));
   };
 
+  const setSelectedCuisines = (selectedCuisines: string[]) => {
+    setSearchState((prevState) => ({
+      ...prevState,
+      selectedCuisines,
+      page: 1,
+    }));
+  };
+
   return (
     <div className="grid grid-cols-1 lg:grid-cols-[250px_1fr] gap-5">
-      <div id="cuisines-list"></div>
+      <div id="cuisines-list">
+        <CuisineFilter
+          selectedCuisines={searchState.selectedCuisines}
+          onChange={setSelectedCuisines}
+          isExpanded={isExpanded}
+          onExpandedClick={() =>
+            setIsExpanded((prevIsExpanded) => !prevIsExpanded)
+          }
+        />
+      </div>
 
       <div id="main-content" className="flex flex-col gap-5">
         <SearchBar
